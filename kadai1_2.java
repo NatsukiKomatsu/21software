759b5e1498df5e403f4b091eae7af30d7bd33b55
NatsukiKomatsu <75403521+NatsukiKomatsu@users.noreply.github.com>
2021-04-16 20:30:15 +0900

    Add files via upload

---
+++ kadai1_2.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class kadai1_2{
+  public static void main(String[]args){
+    System.out.println("好きな食べ物：バームクーヘン");
+    System.out.println("その理由：しっとりしていて甘くて美味しいから。");
+  }
+}
